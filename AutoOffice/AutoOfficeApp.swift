2a67c27f68ea557ca56c78a6627437906b5f45a6
Drsgon13 <ip@sheremetev.info>
2021-09-10 11:55:21 +0300

    Initial commit

---
+++ AutoOffice/AutoOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOfficeApp.swift
+//  AutoOffice
+//
+//  Created by Илья Проскурнев on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AutoOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
